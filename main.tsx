15920c59d97dd8bc8c8e4355b1da2bf1c1cfab80
ferbargar99@hotmail.com <ferbargar99@hotmail.com>
2024-02-25 22:25:35 -0600

    1

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Card } from './Card.tsx'
+import { Card } from './src/Card.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
